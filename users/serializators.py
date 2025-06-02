174a2567e7334f5120f0fd24f7e958c407afe4c5
Руслан <fantom203232@gmail.com>
2025-06-02 13:01:50 +0500

    Update serializators.py

--- users/serializators.py
+++ users/serializators.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import make_password
+
 
 class UserModelSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,40 +10,37 @@ class UserModelSerializer(serializers.ModelSerializer):
             "username", "first_name", 
             "last_name", "email", "password"
         ]
-        # exclude = ["username"]
+        # exclude = ["password", "groups", "user_permissions"]
+
+    # def save(self, **kwargs):
+    #     raw_password: str | None = kwargs.get("password")
+    #     if raw_password:
+    #         kwargs["password"] = make_password(password=raw_password)
+    #     return super().save(**kwargs)
+
+    def create(self, validated_data):
+        validated_data["password"] = make_password(validated_data["password"])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if "password" in validated_data:
+            validated_data["password"] = make_password(validated_data["password"])
+        return super().update(instance, validated_data)
 
 
-# class UserSerializer(serializers.Serializer):
-#     username = serializers.CharField(
-#         required=True,
-#         read_only=True,
-#         max_length=50
-#     )
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True, max_length=50)
+    password = serializers.CharField(write_only=True, min_length=8)
+    first_name = serializers.CharField(required=True, max_length=50)
+    last_name = serializers.CharField(required=True, max_length=50)
+    email = serializers.EmailField(required=True)
 
-#     def validate(self, attrs):
-#         if len(attrs) < 8:
-#             raise ValueError
-#         return super().validate(attrs)
-
-
-class ChangePasswordSerializer(serializers.Serializer):
-    current_password = serializers.CharField(required=True)
-    new_password = serializers.CharField(required=True)
-    new_password_repeat = serializers.CharField(required=False)
+    def validate_username(self, value):
+        if "admin" in value.lower():
+            raise serializers.ValidationError("Username cannot contain 'admin'")
+        return value
 
     def validate(self, attrs):
-        user = self.context["request"].user
-        current_password = attrs.get("current_password")
-        new_password = attrs.get("new_password")
-        new_password_repeat = attrs.get("new_password_repeat")
-
-        if not check_password(current_password, user.password):
-            raise serializers.ValidationError({"current_password": "Current password is incorrect."})
-
-        if new_password_repeat is not None and new_password != new_password_repeat:
-            raise serializers.ValidationError({"new_password_repeat": "Passwords do not match."})
-
-        if current_password == new_password:
-            raise serializers.ValidationError({"new_password": "New password must be different from current password."})
-
+        if attrs["username"] == attrs.get("password"):
+            raise serializers.ValidationError("Password cannot be the same as username")
         return attrs
