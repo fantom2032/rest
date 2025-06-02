d932b09e5a7f4ebf2df15cbf350212837ebed60e
Руслан <fantom203232@gmail.com>
2025-06-02 13:01:04 +0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,84 +1,68 @@
-from rest_framework.request import Request
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
+from rest_framework.exceptions import MethodNotAllowed, PermissionDenied
 from rest_framework import status
-from users.serializators import UserModelSerializer, ChangePasswordSerializer
-from django.db.models.query import QuerySet
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.hashers import make_password
-from rest_framework.decorators import action
 
-from users.serializators import UserModelSerializer
+from users.serializers import UserModelSerializer
 
+class RegistrationAPIView(APIView):
+    permission_classes = [AllowAny]
 
-class UserViewSet(ViewSet):
-    queryset: QuerySet = User.objects.all()
-    serializer_class = UserModelSerializer
-
-    def list(self, request: Request) -> Response:
-        serializer = UserModelSerializer(
-            self.queryset, many=True
-        )
-        if not serializer.data:
-            return Response(
-                status=status.HTTP_404_NOT_FOUND, 
-                data={"error": "users not found"}
-            )
-        return Response(
-            data=serializer.data, 
-            status=status.HTTP_200_OK
-        )
-
-    def create(
-        self, request: Request
-    ) -> Response:
+    def post(self, request):
         s = UserModelSerializer(data=request.data)
         s.is_valid(raise_exception=True)
         try:
-            User.objects.create(
-                username=s.validated_data.get("username"),
-                first_name=s.validated_data.get("first_name"),
-                last_name=s.validated_data.get("last_name"),
-                email=s.validated_data.get("email"),
-                password=make_password(
-                    s.validated_data.get("password")
-            ))
-            
+            User.objects.create_user(**s.validated_data)
             return Response(
-                data={"message": "success"},
-                status=status.HTTP_200_OK
+                data={"message": "User successfully registered"},
+                status=status.HTTP_201_CREATED
             )
         except Exception as e:
             return Response(
-                data={"error": str(e)}, 
-                status=status.HTTP_400_BAD_REQUEST
+                data={"error": str(e)}, status=status.HTTP_400_BAD_REQUEST
             )
-            
-    def retrieve(
-        self, request: Request, pk=None
-    ) -> Response:
-        pass
 
-    def update(
-        self, request: Request, pk=None
-    ) -> Response:
-        pass
+class UserListAPIView(APIView):
+    permission_classes = [IsAuthenticated]
 
-    def partial_update(
-        self, request: Request, pk=None
-    ) -> Response:
-        pass
+    def get(self, request):
+        queryset = User.objects.all()
+        serializer = UserModelSerializer(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    def destroy(
-        self, request: Request, pk=None
-    ) -> Response:
-        pass
-    @action(detail=False, methods=["post"], url_path="change-password")
-    def change_password(self, request):
-        serializer = ChangePasswordSerializer(data=request.data, context={"request": request})
+class UserDetailAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserModelSerializer(user)
+        return Response(data=serializer.data)
+
+    def put(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        if request.user.pk != user.pk:
+            raise PermissionDenied(detail="you have no power here")
+        serializer = UserModelSerializer(instance=user, data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = request.user
-        user.password = make_password(serializer.validated_data["new_password"])
-        user.save()
-        return Response({"message": "Password changed successfully."}, status=status.HTTP_200_OK)    
+        serializer.save(**serializer.validated_data)
+        return Response(data={"message": "user updated"})
 
+    def patch(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        if request.user.pk != user.pk:
+            raise PermissionDenied(detail="you have no power here")
+        serializer = UserModelSerializer(instance=user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(**serializer.validated_data)
+        return Response(data={"message": "user partial updated"})
+
+    def delete(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        if request.user.pk != user.pk:
+            raise PermissionDenied(detail="you have no power here")
+        user.delete()
+        return Response(data={"message": "user has been deleted"})
